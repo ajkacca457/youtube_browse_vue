15e94a486b02f9075dcdfd1a10039e49b06bba69
Avijit Karmaker <ajkacca@gmail.com>
2021-05-14 10:59:09 +0300

    styling item

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,56 @@
 <template>
-<div class="app">
-  <Navbar/>
-  <SearchBar @inputChange="searchVideo"/>
-  <VideoList :videos="list"/>
+  <div class="app">
+    <Navbar />
+    <SearchBar @inputChange="searchVideo" />
+    <VideoList :videos="list" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import SearchBar from './components/SearchBar.vue';
-import VideoList from './components/VideoList.vue';
-import Navbar from './components/NavBar.vue';
-import {API_KEY}from "./private.js";
-
+import SearchBar from "./components/SearchBar.vue";
+import VideoList from "./components/VideoList.vue";
+import Navbar from "./components/NavBar.vue";
+import { API_KEY } from "./private.js";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SearchBar,
     VideoList,
-    Navbar
+    Navbar,
   },
 
-  data(){
-
+  data() {
     return {
-      list: []
-    }
-
+      list: [],
+    };
   },
 
   methods: {
     searchVideo(input) {
-      console.log(input,this.list);
-      axios.get("https://www.googleapis.com/youtube/v3/search", {
-        params:{
-          key:API_KEY,
-          type:"video",
-          part: "snippet",
-          q: input 
-        }
-
-      }).then((response)=> {
-          this.list=response.data.items;
+      console.log(input, this.list);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: input,
+          },
+        })
+        .then((response) => {
+          this.list = response.data.items;
           console.log(this.list);
-      });
-
+        });
     },
-
-  }
-
-
-
-}
+  },
+};
 </script>
 
 <style>
-
-  .app {
-    width: 80%;
-    margin: 2% auto;
-  }
-
+.app {
+  width: 80%;
+  margin: 2% auto;
+}
 </style>
--- src/components/VideoItem.vue
+++ src/components/VideoItem.vue
@@ -1,6 +1,17 @@
 <template>
 
-<h5>{{video.snippet.title}}</h5>
+<li class="list-group-item my-2 border-1 rounded item">
+<div class="media">
+  <img class="mr-3" :src="thumbNail" alt="Generic placeholder image" >
+  <div class="media-body mt-2">
+    <h5 class="mt-0">{{video.snippet.title}}</h5>
+    <p class="mt-0">{{video.snippet.description}}</p>
+    <h6 class="title">Published By: {{video.snippet.channelTitle}}</h6><br>
+    <span>{{video.snippet.publishTime}}</span>
+  </div>
+</div>
+    
+</li>
 
 </template>
 
@@ -11,6 +22,11 @@ export default{
     name:"VideoItem",
     props:{
         video:Object
+    },
+    computed:{
+      thumbNail(){
+          return this.video.snippet.thumbnails.default.url
+      }  
     }
 }
 
@@ -18,6 +34,17 @@ export default{
 </script>
 
 
-<style>
+<style scoped>
+
+.title {
+    margin:-10px 0px;
+}
+
+.item:hover {
+
+    background-color:gainsboro;
+    cursor:pointer;
+}
 
 </style>
+
